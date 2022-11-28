bd7c25e591ab1510c7457c1c424043c037b0641c
philipbenn <phil658c@stud.kea.dk>
2022-11-28 12:11:49 +0100

    printlist

--- Member.java
+++ Member.java
@@ -7,6 +7,12 @@ public class Member {
     private int age;
     private String name;
     private boolean isCompetitive;
+    private String[] menuItems = new String[]{
+            "1. Junior swimmers. \n" +
+                    "2. Senior swimmers. \n" +
+                    "3. Adult swimmers. \n" +
+                    "4. Competitive swimmers over 18. \n" +
+                    "5. Competitive swimmers under 18."};
 
     //Instances
     ArrayList<Member> competitiveTeamOverEighteen = new ArrayList<>();
@@ -14,6 +20,8 @@ public class Member {
     ArrayList<Member> juniorSwimmers = new ArrayList<>();
     ArrayList<Member> seniorSwimmers = new ArrayList<>();
     ArrayList<Member> adultSwimmers = new ArrayList<>();
+
+    Menu memberLists = new Menu("MEMBER LISTS: ", "Please choose: ", menuItems);
 
     //Setters
     public void setName(String name) {
@@ -42,7 +50,7 @@ public class Member {
         return isCompetitive;
     }
 
-    //Constructor
+    //Constructors
     public Member(String name, int age, boolean isCompetitive) {
         setName(name);
         setAge(age);
@@ -65,8 +73,8 @@ public class Member {
         age = scanner.nextInt();
         scanner.nextLine();
         setAge(age);
-        checkAge();
 
+        checkAge();
         checkCompetitive();
     }
 
@@ -82,7 +90,7 @@ public class Member {
         }
     }
 
-    public void checkCompetitive() {
+    public void checkCompetitive() { //TODO FIX COMPETITIVE BUG
         Scanner scanner = new Scanner(System.in);
         System.out.println("Is member competitive? \n 1. Yes \n 2. No");
         String competitive = scanner.nextLine();
@@ -105,11 +113,39 @@ public class Member {
     }
 
     public void printList() {
-        for (Member member : competitiveTeamUnderEighteen) {
-            System.out.println(member);
-        }
-        for (Member member : competitiveTeamOverEighteen) {
-            System.out.println(member);
+        memberLists.printMenu();
+        int chooseList = memberLists.readChoice();
+
+        switch (chooseList) {
+            case 1 -> {
+                for (Member member : juniorSwimmers) {
+                    System.out.println(member);
+                }
+            }
+            case 2 -> {
+                for (Member member : seniorSwimmers) {
+                    System.out.println(member);
+                }
+            }
+            case 3 -> {
+                for (Member member : adultSwimmers) {
+                    System.out.println(member);
+                }
+            }
+            case 4 -> {
+                for (Member member : competitiveTeamOverEighteen) {
+                    System.out.println(member);
+                }
+            }
+            case 5 -> {
+                for (Member member : competitiveTeamUnderEighteen) {
+                    System.out.println(member);
+                }
+            }
+            default -> {
+                System.out.println("Invalid input.");
+                memberLists.readChoice();
+            }
         }
 
 
